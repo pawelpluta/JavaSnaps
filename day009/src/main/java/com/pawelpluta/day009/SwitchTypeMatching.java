28e51b94ccdec9d28b5f61afbd0f2df53a8c1c4e
pawelpluta <pawel.pluta.katowice@gmail.com>
2023-01-11 07:30:28 +0100

    Day 9 solution

--- day009/src/main/java/com/pawelpluta/day009/SwitchTypeMatching.java
+++ day009/src/main/java/com/pawelpluta/day009/SwitchTypeMatching.java
@@ -2,6 +2,14 @@ package com.pawelpluta.day009;
 
 class SwitchTypeMatching {
     MatchResults match(Object value) {
-        return null;
+        return switch (value) {
+            case null -> MatchResults.forNull();
+            case Integer casted -> MatchResults.forInteger();
+            case Long casted -> MatchResults.forLong();
+            case Character casted -> MatchResults.forChar();
+            case String casted -> MatchResults.forString();
+            case CharSequence casted -> MatchResults.forCharSequence();
+            default -> throw new IllegalArgumentException("Unsupported value: " + value);
+        };
     }
 }
